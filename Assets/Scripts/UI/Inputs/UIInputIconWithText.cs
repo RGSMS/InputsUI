aba0569c9a951b6ac5f973384deda12edbdb0dda
Rômulo Gomes de Souza Marques dos Santos <romulomarquessantos@hotmail.com>
2024-02-14 14:22:30 -0300

    Add sufflix info to input icon System

--- Assets/Scripts/UI/Inputs/UIInputIconWithText.cs
+++ Assets/Scripts/UI/Inputs/UIInputIconWithText.cs
@@ -8,11 +8,21 @@ namespace RGSMS.UI
         [SerializeField]
         private TextMeshProUGUI _text = null;
 
+        [SerializeField]
+        private string _uiSuffix = null;
+
+        public void SetUISuffix(string suffix) => _uiSuffix = suffix;
+
         public override void HandleData()
         {
             base.HandleData();
 
             string text = _inputManager.GetInputLocKey(Data);
+            if (!string.IsNullOrEmpty(_uiSuffix))
+            {
+                text = string.Concat(text, _uiSuffix);
+            }
+
             //falta localizar o texto
             _text.SetText(text);
         }
